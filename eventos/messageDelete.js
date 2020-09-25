05dfd0659c33cbac453120cff675ec87e34171e6
luarrekcah <64855653+luarrekcah@users.noreply.github.com>
2020-09-25 09:56:01 -0500

    Evento
    Veja a mensagem que foi apagada.

---
+++ eventos/messageDelete.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports = async (bot, message) => {
+  /*
+  let logCH = await bot.channels.cache.get("744017110518464612");
+  const embed = new Discord.MessageEmbed()
+    .setColor("RED")
+    .setTitle("Mensagem deletada!")
+    .setDescription(
+      "O usuário <@" +
+        message.author +
+        "> deletou a mensagem " +
+        message.content
+    );
+
+  bot.channels.cache.get(logCH).send(embed);
+  */
+};
